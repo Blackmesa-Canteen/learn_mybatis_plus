38e3f53dea8d73eefc001efe1673d029ad211847
996Worker <69796042+Blackmesa-Canteen@users.noreply.github.com>
2022-01-07 11:13:22 +0800

    Initial commit

---
+++ src/test/java/com/learn/learn_mybatis_plus/LearnMybatisPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.learn_mybatis_plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnMybatisPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
